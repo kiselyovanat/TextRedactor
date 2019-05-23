4bddea1bc283b159421e15d3f93932e86bdc0100
Natasha <kiselyov-natalya@mail.ru>
2019-05-23 12:10:36 +0700

    [+] copy,paste

--- redactor.py
+++ redactor.py
@@ -7,6 +7,7 @@ class Document(object):
     def __init__(self,text):
         self.size = len(text)
         self.text = text
+        self.buffer = None
 
     def show(self):
         print self.text
@@ -61,55 +62,104 @@ class DeleteCommand(Command):
     def DeleteCommand(self,startPosition, endPosition):
         self.startPosition = startPosition
         self.endPosition = endPosition
+        self.deletedText = self.doc.text[self.startPosition:self.endPosition]
 
     def execute(self):
         self.doc.text = self.doc.text[:self.startPosition] + self.doc.text[self.endPosition:]
 
+    def unexecute(self):
+        #print 'print ',self.doc.text
+        self.doc.text = self.doc.text[:self.startPosition] + self.deletedText + self.doc.text[self.startPosition:]
+
+class CopyCommand(Command):
+    def __init__(self,doc):
+        self.doc = doc
+        self.startPosition = None
+        self.endPosition = None
+
+    def CopyCommand(self,startPosition, endPosition):
+        self.startPosition = startPosition
+        self.endPosition = endPosition
+
+    def execute(self):
+        self.doc.buffer = self.doc.text[self.startPosition:self.endPosition]
+
+    #def unexecute(self):
+    #    self.doc.buffer = None
+
+class PasteCommand(Command):
+    def __init__(self,doc):
+        self.doc = doc
+        self.position = None
+
+    def PasteCommand(self,position):
+        self.position = position
+
+    def execute(self):
+        if self.doc.buffer:
+            self.doc.text = self.doc.text[:self.position] + self.doc.buffer + self.doc.text[self.position:]
+        else:
+            print 'Nothing to paste'
+
+    def unexecute(self):
+        index = self.position + len(self.doc.buffer)
+        self.doc.text =  self.doc.text[:self.position] + self.doc.text[index:]
+
+
+
 class Receiver(object):
-    def __init__(self, insert, delete, undo, redo):
+    def __init__(self, insert, delete, undo, redo, copy, paste):
         self.insert_command = insert
         self.undo_command = undo
         self.redo_command = redo
         self.delete_command = delete
+        self.copy_command = copy
+        self.paste_command = paste
 
         self.command_stack = []
         self.redo_stack = []
-        self.current_command = None
 
     def insert(self,text,position):
-        self.current_command = self.insert_command
-        self.current_command.InsertCommand(text,position)
-        self.current_command.execute()
-        self.command_stack.append(self.current_command)
+        self.insert_command.InsertCommand(text,position)
+        self.insert_command.execute()
+        self.command_stack.append(self.insert_command)
 
     def undo(self):
-        self.current_command = self.undo_command
         command = self.command_stack.pop()
         self.redo_stack.append(command)
         if command:
-            self.current_command.execute(command)
+            self.undo_command.execute(command)
 
         else:
             print "Nothing to undo"
 
     def redo(self):
-        self.current_command = self.redo_command
         command = self.redo_stack.pop()
+        self.command_stack.append(command)
         if command:
-            self.current_command.execute(command)
+            self.redo_command.execute(command)
         else:
             print "Nothing to redo"
 
     def delete(self,startPosition, endPosition):
-        self.current_command = self.delete_command
-        self.current_command.DeleteCommand(startPosition, endPosition)
-        self.current_command.execute()
+        self.delete_command.DeleteCommand(startPosition, endPosition)
+        self.delete_command.execute()
+        self.command_stack.append(self.delete_command)
 
+    def copy(self, startPosition, endPosition):
+        self.copy_command.CopyCommand(startPosition, endPosition)
+        self.command_stack.append(self.copy_command)
+        self.copy_command.execute()
+
+    def paste(self, position):
+        self.paste_command.PasteCommand(position)
+        self.command_stack.append(self.paste_command)
+        self.paste_command.execute()
 
 
 doc = Document('natasha')
 doc.show()
-receiver = Receiver(InsertCommand(doc), DeleteCommand(doc), UndoCommand(), RedoCommand())
+receiver = Receiver(InsertCommand(doc), DeleteCommand(doc), UndoCommand(), RedoCommand(), CopyCommand(doc), PasteCommand(doc))
 receiver.insert('love',3)
 #receiver.insert('love',3)
 doc.show()
@@ -117,8 +167,19 @@ receiver.undo()
 doc.show()
 receiver.redo()
 doc.show()
-receiver.delete(3,7)
+receiver.delete(0,4)
 doc.show()
+receiver.undo()
+doc.show()
+receiver.redo()
+doc.show()
+receiver.copy(0,4)
+print 'buffer ', doc.buffer
+receiver.paste(0)
+doc.show()
+receiver.undo()
+doc.show()
+
 '''    Реализовать примитивный текстовый редактор, способный производить ряд операций над одной строкой, с использование шаблона проектирования “Команда”
     Тесктовый редактор работает с двумя текстовыми файлами. В первом находится строка, над которой он проводит все операции. Во втором - последовательность команд, которые нужно выполнить. Необходимо реализовать поддержку следующих команд:
     • copy idx1 idx2 - скопировать в буффер обмена символы с позиции idx1 до позиции idx2
